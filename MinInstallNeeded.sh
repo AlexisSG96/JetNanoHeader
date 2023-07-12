4243c15dc9e2a7328a9357f150e471c56292b7f3
AlexisSG96 <32774104+AlexisSG96@users.noreply.github.com>
2023-07-12 07:58:45 -0700

    Update MinInstallNeeded.sh
    Updated Initialize Script

--- MinInstallNeeded.sh
+++ MinInstallNeeded.sh
@@ -1,5 +1,13 @@
 export current=$PWD
 
+# Make sure you can use apt
+
+if grep -q "nameserver 8.8.8.8" /etc/resolv.conf; then
+	echo "/etc/resolv.conf is good to go!"
+else
+	echo "nameserver 8.8.8.8" | tee -a /etc/resolv.conf >/dev/null
+fi
+
 # Useful packages
 
 sudo apt-get update -y
@@ -35,9 +43,18 @@ dtc -@ -I dts -O dtb -o overlays/jetsonnano/tegra210-p3448-0000-p3449-0000-b00-h
 
 sudo cp overlays/jetsonnano/tegra210-p3448-0000-p3449-0000-b00-hdr40.dtbo /boot/
 
-sudo /opt/nvidia/jetson-io/config-by-hardware.py -n "Jetson 40pin Header"
-
 sudo /opt/nvidia/jetson-io/config-by-hardware.py -l
+
+output=$(sudo /opt/nvidia/jetson-io/config-by-hardware.py -l)
+
+regex="User Custom \[[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}\]"
+
+if [[ "$output" =~ $regex]]; then
+	extracted_content=${BASH_REMATCH[0]}
+	echo "Extracted content: $extracted_content"
+else
+	echo "User custom value not found. (Error: /opt/nvidia/jetson-io/config-by-hardware.py -l)"
+fi
 
 # Clone eMMC
 cd $current
@@ -59,7 +76,49 @@ sudo apt-get install -y v4l-utils
 
 sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
+# Comment out problem that keeps gstreamer from building
+# Using following command: sed -i 's|pattern|replacement|'
+# Using pipe character as the delimiter avoids issues with special characters
+# ^ represents start of line | .* matches any character | $ represents end of the line
+sed -i 's|^.*gst_rtsp_media_factory_set_enable_rtcp (factory, !disable_rtcp);$|  //gst_rtsp_media_factory_set_enable_rtcp (factory, !disable_rtcp);|' gst-rtsp-server/examples/test-launch.c
+
+# Compile gstreamer test-launch
+gcc gst-rtsp-server/examples/test-launch.c -o test-launch $(pkg-config --cflags --libs gstreamer-1.0 gstreamer-rtsp-server-1.0)
+
+# Function to run gstreamer
+
+run_gstreamer() 
+{
+
+# Check for devices
+
+# Doesn't work as output is stderr - hello
+# output=$(v4l2-ctl --list-devices)
+# if echo "$output" | grep -Fq "Failed to open /dev/video0: No such file or directory"; then
+
+#'2>&1' redirects the stderr to stdout
+output=$(v4l2-ctl --list-devices 2>&1)
+
+if [[ "$output" == "Failed to open /dev/video0: No such file or directory"* ]]; then
+# Something else went wrong
+	echo "$output"
+	exit 0
+else
+#Run test-launch
+	./test-launch "v4l2src device=/dev/video0 io-mode=2 ! image/jpeg,width=1280,height=720 ! nvjpegdec ! video/x-raw ! nvvidconv ! nvv412h264enc ! h264parse ! rtph264pay ! name=pay0 pt=96"
+fi
+
+}
+
+# Function to run GST-Launch
+run_gst_launch()
+{
+	gst-launch-1.0 v4l2src device=/dev/video0 io-mode=2 ! image/jpeg,width=1280,height=720 ! nvjpegdec ! video/x-raw ! nvvidconv ! 'video/x-raw(memory=NVVM),format=I420' ! nvoverlaysink
+}
+
 # Install Jetson.GPIO (Rasberry PI Alternative for 40 Pin Header)
+cd $current
+
 sudo groupadd -f -r gpio
 
 sudo usermod -a -G gpio "$(whoami)"
@@ -67,10 +126,29 @@ sudo usermod -a -G gpio "$(whoami)"
 sudo -H pip3 install Jetson.GPIO
 
 # Install MavLink and MavProxy
+cd $current
+
 sudo apt-get install -y python-pip python3-pip
 
 sudo apt-get install -y python3-dev python3-opencv python3-wxgtk4.0 python3-matplotlib python3-lxml libxml2-dev libxslt-dev
 
-sudo pip3 install PyYAML mavproxy
+sudo -H pip3 install PyYAML mavproxy
 
-#sudo mavproxy.py --master=/dev/ttyTHS1
+# Function to run MavProxy
+
+run_mavproxy()
+{
+	sudo mavproxy.py --master=/dev/ttyTHS1
+}
+
+# Install NVIDIA Jetson Device for the command: jtop
+cd $current
+
+sudo -H pip3 install -U jetson-stats
+
+# Get OpenCV Install Script and Examples
+cd $current
+
+git clone https://github.com/JetsonHacksNano/buildOpenCV.git
+
+sudo apt-get install -y python3-dev python3-numpy python3-py python3-pytest
